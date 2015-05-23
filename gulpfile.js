26fecf1263b8fa40b203acdedc0d4a84e7e9784e
tjallingt <tjallingt@gmail.com>
2015-05-23 22:30:40 +0200

    Updated gulpfile so the linter is run before the uglifier.

--- gulpfile.js
+++ gulpfile.js
@@ -11,30 +11,30 @@ gulp.task('clear-dist', function() {
 
 // copy all files to build directory
 gulp.task('copy-all', function() {
-	gulp.src(['src/**', '!src/include/js/*.js', '!src/include/css/*.scss'], {dot: true})
+	return gulp.src(['src/**', '!src/include/js/*.js', '!src/include/css/*.scss'], {dot: true})
 		.pipe(gulp.dest('dist'));
 });
 
 // minify javascript files
-gulp.task('minify-js', function() {
-	gulp.src(['src/include/js/*.js'], {base: 'src'})
+gulp.task('minify-js', ['lint-js'], function() {
+	return gulp.src(['src/include/js/*.js'], {base: 'src'})
 		.pipe(uglify())
 		.pipe(gulp.dest('dist'));
 });
 
 // lint certain javascript files (non-libraries)
 gulp.task('lint-js', function() {
-	gulp.src(['src/include/js/*.js'])
+	return gulp.src(['src/include/js/*.js'])
 		.pipe(jshint({curly: true, jquery: true}))
 		.pipe(jshint.reporter('default'));
 });
 
 // minify css files
 gulp.task('minify-css', function() {
-	gulp.src(['src/include/css/*.scss'], {base: 'src'})
+	return gulp.src(['src/include/css/*.scss'], {base: 'src'})
 		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
 		.pipe(gulp.dest('dist'));
 });
 
 // default sets up entire project
-gulp.task('default', ['copy-all', 'minify-js', 'lint-js', 'minify-css']);
+gulp.task('default', ['copy-all', 'minify-js', 'minify-css']);
