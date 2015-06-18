c9f15e27e255c13af491d78095311972c074a3b3
tjallingt <tjallingt@gmail.com>
2015-06-18 16:33:43 +0200

    Changed the javascript structure to use a Class for the website

--- src/include/js/main.js
+++ src/include/js/main.js
@@ -1,135 +1,139 @@
-/* GLOBAL VARIABLES */
-var loading = false;
-var hasMouse = false;
-var fab = document.querySelector( "#fab" );
-// function for looping over querySelectorAll stuff
-var forEach = function( array, callback, scope ) {
-	for ( var i = 0; i < array.length; i++ ) {
-		callback.call( scope, array[i] );
+class Website {
+	constructor() {
+		// class wide variables
+		this.loading = false;
+		this.hasMouse = false;
+
+		// insert calculated age
+		document.querySelector( "#age" ).innerHTML = `${this.calculateAge( "1994-5-18" )} years old`;
+
+		// if a mobile use clicks a link mousemove is triggered, to prevent this touchstart will unbind the mousemove listener
+		// (only works if the touchstart is triggered before mousemove which seems to be the case)
+		document.addEventListener( "mousemove", ( event ) => this.noTouch( event ) );
+		document.addEventListener( "touchstart", ( event ) => {
+			document.removeEventListener( "mousemove",  ( event ) => this.noTouch( event ) );
+		});
+
+		/* FLOATING ACTION BUTTON */
+		// move button off screen
+		if( window.scrollY < 50 ) {
+			fab.classList.add( "fab-hidden" );
+			fab.style.bottom = "-" + getComputedStyle( fab ).height;
+		}
+
+		// show/hide button when scrolling
+		window.addEventListener( "scroll", ( event ) => {
+			if( window.scrollY > 50 ) {
+				fab.classList.remove( "fab-hidden" );
+				fab.style.bottom =  "";
+			}
+			else if( !fab.classList.contains( "fab-hidden" ) ) {
+				fab.classList.add( "fab-hidden" );
+				fab.style.bottom = "-" + getComputedStyle( fab ).height;
+			}
+		});
+
+		if (history.pushState) {
+
+			// create initial state for history api
+			this.getJson( location.href, ( json ) => {
+				history.replaceState( json, "", location.href );
+			});
+
+			// add eventlisteners for the history api functions
+			this.forEach( document.querySelectorAll( ".pagenav" ), ( element ) => {
+				element.addEventListener( "click",  ( event ) => this.navPushState( event ) );
+			});
+
+			// restore window after popstate
+			window.addEventListener( "popstate", ( event ) => {
+				this.renderPage( event.state );
+				ga( "send", "pageview", location.pathname );
+			});
+
+			// stop the logo from spinning when in the correct orientation
+			this.forEach( document.querySelectorAll( ".logo" ), ( element ) => {
+				element.addEventListener( "animationiteration", ( event ) => {
+					if( !this.loading ) {
+						event.currentTarget.classList.remove( "spin" );
+					}
+				});
+			});
+
+		}
 	}
-};
 
-/* HTML AND CSS CHANGES */
-// calculate age
-var dob = new Date( "1994-5-18" );
-var today = new Date();
-var age = Math.floor( ( today - dob ) / ( 365.25 * 24 * 60 * 60 * 1000 ) ); // days * hours * minutes * seconds * milliseconds
-document.querySelector( "#age" ).innerHTML = age + " years old";
-
-// if a user uses mouse hide titles and show on hover
-function noTouch( event ) {
-	hasMouse = true;
-	forEach( document.querySelectorAll( ".title" ), function( element ) {
-		element.classList.add( "no-touch" );
-	});
-	document.removeEventListener( "mousemove", noTouch );
-}
-
-// if a mobile use clicks a link mousemove is triggered, to prevent this touchstart will unbind the mousemove listener
-// (only works if the touchstart is triggered before mousemove which seems to be the case)
-document.addEventListener( "mousemove", noTouch );
-document.addEventListener( "touchstart", function( event ) {
-	document.removeEventListener( "mousemove", noTouch );
-});
-
-/* FLOATING ACTION BUTTON */
-// move button off screen
-if( window.scrollY < 50 ) {
-	fab.classList.add( "fab-hidden" );
-	fab.style.bottom = "-" + getComputedStyle( fab ).height;
-}
-
-// show/hide button when scrolling
-window.addEventListener( "scroll", function( event ) {
-	if( window.scrollY > 50 ) {
-		fab.classList.remove( "fab-hidden" );
-		fab.style.bottom =  "";
+	// function to calculate the age for a given date of birth
+	calculateAge( dateOfBirth ) {
+		let dob = new Date( dateOfBirth );
+		let today = new Date();
+		let age = new Date( today - dob ).getFullYear() - 1970; // 1970 is the beginning of unix time
 	}
-	else if( !fab.classList.contains( "fab-hidden" ) ) {
-		fab.classList.add( "fab-hidden" );
-		fab.style.bottom = "-" + getComputedStyle( fab ).height;
+
+
+	// if a user uses mouse hide titles and show on hover
+	noTouch( event ) {
+		this.hasMouse = true;
+		this.forEach( document.querySelectorAll( ".title" ), ( element ) => {
+			element.classList.add( "no-touch" );
+		});
+		document.removeEventListener( "mousemove", this.noTouch );
 	}
-});
 
-/* HISTORY API */
-// create initial state
-$.ajax({
-	dataType: "json",
-	url: location.href,
-	cache: false,
-	success: function( json ) {
-		history.replaceState( json, "", location.href );
-	}
-});
-
-// click navigate home
-forEach( document.querySelectorAll( ".pagenav" ), function( element ) {
-	element.addEventListener( "click", navPushState );
-});
-
-// internal navigation
-function navPushState( event ) {
-	if (history.pushState) {
+	// internal navigation
+	navPushState( event ) {
 		event.preventDefault();
 		
-		forEach( document.querySelectorAll( ".logo" ), function( element ) {
+		this.forEach( document.querySelectorAll( ".logo" ), ( element ) => {
 			element.classList.add( "spin" );
 		});
 		
-		loading = true;
-		var url = this.href.replace( location.origin, "" );
+		this.loading = true;
+
+		let url = event.currentTarget.href.replace( location.origin, "" );
 		
+		this.getJson( url, ( json ) => {
+			history.pushState( json, "", url );
+			this.renderPage( json );
+			this.loading = false;
+			ga( "send", "pageview", url );
+		});
+	}
+
+	// render the page from the json data
+	// move fade in/out to css (toggle a class in js)
+	renderPage( json ) {
+		$( "#content" ).fadeOut( 500, () => {
+			$( "#content" ).html( json.data ).fadeIn( 500 );
+			
+			this.forEach( document.querySelector( "#content" ).querySelectorAll( ".pagenav" ), ( element ) => {
+				element.addEventListener( "click",  ( event ) => this.navPushState( event ) );
+			});
+
+			if( this.hasMouse ) {
+				document.querySelector( ".title" ).classList.add( "no-touch" );
+			}
+		});
+	}
+
+	// helper function for looping over nodeLists returned by querySelectorAll
+	forEach( array, callback, scope ) {
+		for( let i = 0; i < array.length; i++ ) {
+			callback.call( scope, array[i] );
+		}
+	}
+
+	// shorthand for the jQuery ajax function
+	// replace with own get request
+	getJson( url, callback ) {
 		$.ajax({
 			dataType: "json",
 			url: url,
 			cache: false,
-			success: function( json ) {
-				history.pushState( json, "", url );
-				renderPage( json );
-				loading = false;
-				ga( "send", "pageview", url );
-			}
+			success: callback
 		});
 	}
 }
 
-// restore window after popstate
-window.addEventListener( "popstate", function( event ){
-	renderPage( event.state );
-	ga( "send", "pageview", location.pathname );
-});
-
-// render the page from the json data
-function renderPage( json ) {
-	$( "#content" ).fadeOut( 500, function() {
-		$( this ).html( json.data ).fadeIn( 500 );
-		
-		forEach( document.querySelector( "#content" ).querySelectorAll( ".pagenav" ), function( element ) {
-			element.addEventListener( "click", navPushState );
-		});
-		if( hasMouse ) {
-			document.querySelector( ".title" ).classList.add( "no-touch" );
-		}
-	});
-}
-
-/*
-* doesnt work atm, test later... (better performance on mobile/in general?)
-* main.css -> #content {transition: opacity 500ms ease;}
-*/
-/*
-$( "#content" ).addClass( "fade" ).one( "transitionend", function() {
-	var content = Mustache.render( templates[json.type], json.data );
-	$( this ).html( content ).removeClass( "fade" );
-	if(hasMouse) {
-		$( ".title" ).addClass( "no-touch" );
-	}
-});
-*/
-
-// stop the logo from spinning when in the correct orientation
-$( ".logo" ).on( "animationiteration webkitAnimationIteration oanimationiteration MSAnimationIteration", function (e) {
-	if( !loading ) {
-		$( this ).removeClass( "spin" );
-	}
-});
+// run the whole thing
+let website = new Website();
